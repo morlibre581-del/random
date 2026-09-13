7d9f035df3c89f3f4643d6ba4bf69f68205dd366
Lidia Shlapakova <morlibre581@gmail.com>
2026-09-13 12:49:44 +0300

    deploy built site

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite"
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  base: "/random/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
